816531ecda4f1ba94c16b3c9022646f6a008e61b
Shivi1998 <sethshivangi1998@gmail.com>
2020-03-09 12:40:52 +0400

    Fixed order of tables.

--- DjangoUnlimited/Home/models.py
+++ DjangoUnlimited/Home/models.py
@@ -5,6 +5,7 @@ from datetime import datetime
 # Create your models here.
 from Admin.models import Admin
 
+
 class Industry(models.Model):
     industry_name = models.CharField(max_length=50)
 
@@ -13,31 +14,31 @@ class Skill(models.Model):
     skill_name = models.CharField(max_length=50)
 
 
-class JobSkill(models.Model):
-    job_id = models.ForeignKey(Job, on_delete=models.CASCADE, related_name='skill_for_job')
-    skill_id = models.ForeignKey(Skill, on_delete=models.CASCADE, related_name='job_skill')
-
-
 class JobType(models.Model):
     type_name = models.CharField(max_length=50)
 
 
 class Job(models.Model):
-    industry_id = models.ForeignKey(Industry, on_delete = models.CASCADE, related_name ='job_industry')
+    industry_id = models.ForeignKey(Industry, on_delete=models.CASCADE, related_name='job_industry')
     duration = models.IntegerField()
     date_posted = models.DateField(null=False, blank=False)
     posted_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='job_author')
-    job_title =  models.CharField(max_length=100)
+    job_title = models.CharField(max_length=100)
     desc = models.TextField()
     job_status = [
-        ('Open','Open'),
-        ('Closed','Closed'),
-        ('Deleted','Deleted')
+        ('Open', 'Open'),
+        ('Closed', 'Closed'),
+        ('Deleted', 'Deleted')
     ]
-    date_closed = models.DateFirl(null=True, blank=True)
+    date_closed = models.DateField(null=True, blank=True)
     location = models.CharField(max_length=100)
-    job_type_id = models.ForeignKey(JobType, on_delete = models.CASCADE, related_name ='job_type')
+    job_type_id = models.ForeignKey(JobType, on_delete=models.CASCADE, related_name='job_type')
     salary = models.FloatField()
+
+
+class JobSkill(models.Model):
+    job_id = models.ForeignKey(Job, on_delete=models.CASCADE, related_name='skill_for_job')
+    skill_id = models.ForeignKey(Skill, on_delete=models.CASCADE, related_name='job_skill')
 
 
 class HelpDeskComplaints(models.Model):
