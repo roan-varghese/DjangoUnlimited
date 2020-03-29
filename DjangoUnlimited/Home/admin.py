68c6d2ddf0423bdd55c7b2a989305b7013d0e2c0
Roan <roan.rv7@gmail.com>
2020-03-29 14:14:09 +0400

    Changed css

--- DjangoUnlimited/Home/admin.py
+++ DjangoUnlimited/Home/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Industry, Skill
+from .models import Industry, Skill, Job, JobType
 
 # Register your models here.
 admin.site.register(Industry)
 admin.site.register(Skill)
+admin.site.register(Job)
+admin.site.register(JobType)
