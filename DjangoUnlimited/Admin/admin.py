5c7af76eb99a2c572f28b41510769c43775679b1
Roan <roan.rv7@gmail.com>
2020-04-06 16:16:18 +0400

    renamed Templates folder to template

--- DjangoUnlimited/Admin/admin.py
+++ DjangoUnlimited/Admin/admin.py
@@ -6,9 +6,139 @@ from django.contrib.auth.models import Group, User
 from django.contrib.sites.models import Site
 from django_comments.models import Comment
 
+
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User, auth
+from django.db import transaction
+from django.contrib.admin.views.decorators import staff_member_required 
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+from django.core.mail import send_mail
+from DjangoUnlimited.settings import SENDGRID_API_KEY
+from Home.forms import CreateJobForm, EditJobForm
+from Employer.forms import InitialEmployerForm, EmployerForm
+# Create your views here.
+
+from .forms import InitialAdminForm, AdminForm, AddIndustryForm
+from Accounts.views import isValidated
+from .models import Admin
+from .forms import EditAdminProfileForm
+from Student.forms import EditStudentProfileForm
+from django.conf.urls import url
+
 class adminAdmin(admin.ModelAdmin):
     change_list_template = "admin/admin_change_list.html"
     change_form_template = "admin/admin_change_form.html"
+
+    def get_urls(self):  
+        custom_urls = [
+            path('create_admin/', self.admin_site.admin_view(self.create_admin), name='A_create_admin'),
+            
+            path('edit_admin_profile', self.edit_profile, name='A_edit_admin_profile'),
+            path('view_admin_profile', self.view_profile, name='A_view_admin_profile'),
+        ]
+        urls = super().get_urls()
+        return custom_urls + urls
+
+    def create_admin(self, request):
+        if request.method == 'POST':
+            user_form = InitialAdminForm(request.POST)
+
+            if user_form.is_valid():
+                if user_form.usernameExists():
+                    messages.info(request, 'Username already taken. Try a different one.')
+                    return redirect("admin_register")
+
+                elif user_form.emailExists():
+                    messages.info(request, 'Email already taken. Try a different one.')
+                    return redirect("admin_register")
+
+                elif not user_form.samePasswords():
+                    messages.info(request, 'Passwords not matching. Try again.')
+                    return redirect("admin_register")
+
+                elif not user_form.emailDomainExists():
+                    messages.info(request, 'Email domain does not exist. Try again.')
+                    return redirect("admin_register")
+
+                else:
+                    if isValidated(user_form.cleaned_data.get('password1')):
+                        admin_form = AdminForm(request.POST, request.FILES)
+
+                        if admin_form.is_valid():
+                            with transaction.atomic():
+                                user = user_form.save()
+                                admin = admin_form.save(commit=False)
+                                admin.user = user
+                                admin.save()
+                                next_page = request.POST.get('next', '/')
+                                return redirect(next_page)
+                        else:
+                            messages.info(request, admin_form.errors)
+                            return redirect("admin_register")
+                    else:
+                        messages.info(request,'ERROR: Password must be 8 characters or more, and must have at least 1 uppercase, lowercase, numeric and special character.')
+                        return redirect("admin_register")
+            else:
+                messages.info(request, user_form.errors)
+                return redirect("admin_register")
+        else:
+            user_form = InitialAdminForm()
+            admin_form = AdminForm()
+            args = {'admin_form': admin_form, 'user_form': user_form}
+            return render(request, 'admin/admin_registration.html', args)
+
+    def view_profile(self, request):
+        user = request.user
+        hasAdminDetails = True
+        try:
+            admin = Admin.objects.get(user_id=user.id)
+            args = {'admin': admin, 'user': user, 'hasAdminDetails': hasAdminDetails}
+            return render(request, 'admin/view_admin_profile.html', args)
+        except:
+            if request.method == 'POST':
+                admin_form = AdminForm(request.POST, request.FILES)
+
+                if admin_form.is_valid():
+                    with transaction.atomic():
+                        admin = admin_form.save(commit=False)
+                        admin.user = user
+                        admin.save()
+                        return redirect(request.path_info)
+                else:
+                    messages.info(request, admin_form.errors)
+                    return redirect(request.path_info)
+
+            else:
+                hasAdminDetails = False
+                admin_form = AdminForm()
+                args = {'admin_form': admin_form, 'hasAdminDetails': hasAdminDetails}
+                return render(request, 'admin/view_admin_profile.html', args)
+
+
+    def edit_profile(self, request):
+        admin = Admin.objects.get(user_id=request.user.id)
+
+        if request.method == 'POST':
+            user_form = EditAdminProfileForm(request.POST, instance=request.user)
+            admin_form = AdminForm(request.POST, request.FILES, instance=admin)
+
+            if user_form.is_valid() and admin_form.is_valid():
+                with transaction.atomic():
+                    user_form.save()
+                    admin_form.save()
+                    next_page = request.POST.get('next', '/')
+                    return redirect(next_page)
+            else:
+                messages.info(request, admin_form.errors)
+                messages.info(request, user_form.errors)
+                return redirect(request.path_info)
+        else:
+            user_form = EditAdminProfileForm(instance=request.user)
+            admin_form = AdminForm(instance=admin)
+            args = {'admin_form': admin_form, 'user_form': user_form}
+            return render(request, 'admin/edit_admin_profile.html', args)
 
 # Register your models here.
 admin.site.register(Admin, adminAdmin)
--- DjangoUnlimited/Home/admin.py
+++ DjangoUnlimited/Home/admin.py
@@ -8,6 +8,8 @@ from .models import Industry, Skill, Job, JobType
 class JobAdmin(admin.ModelAdmin):
     list_display = ('posted_by', 'job_title', 'description', 'salary')
     # change_form_template = "admin/job_change_form.html"
+    change_list_template = "admin/job_change_list.html"
+
 
 # Register your models here.
 admin.site.register(Job, JobAdmin)
