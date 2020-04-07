48c44564cd4fb43d1d7d43cc32c13c7ba99fb138
Shivi1998 <sethshivangi1998@gmail.com>
2020-04-07 22:54:39 +0400

    Bulletin changes

--- DjangoUnlimited/Bulletin/views.py
+++ DjangoUnlimited/Bulletin/views.py
@@ -15,18 +15,6 @@ from django.core.paginator import Paginator
 from Accounts.views import get_user_type
 
 
-# The bulletin view for each individual
-class BulletinView(TemplateView):
-    template_name = 'bulletin/base.html'
-
-    def get(self, request, *args, **kwargs):
-        user = get_user_type(request)
-        args = {
-            'user': user,
-        }
-        return render(request, self.template_name, args)
-
-
 # Create the new bulletin posts
 class CreatePostView(TemplateView):
     template_name = 'bulletin/base.html'
@@ -56,69 +44,6 @@ class CreatePostView(TemplateView):
                     post.release_date = timezone.now()
                     post.update_date = timezone.now()
                     post.status = True  # post is published for everyone to view
-                    post.save()
-                    return redirect('post_new')
-            # check if post is saved in draft mode
-            elif request.POST.get("savebutton"):
-                if form.is_valid():
-                    post = form.save(commit=False)
-                    post.author = request.user
-                    post.author_updated = request.user
-                    post.release_date = timezone.now()
-                    post.update_date = timezone.now()
-                    post.status = False  # post is in draft mode
-                    post.save()
-                    return redirect('post_new')
-
-
-# Edit previously published bulletin posts
-class EditBulletin(TemplateView):
-    template_name = 'bulletin/post_edit.html'
-
-    def get(self, request, *args, **kwargs):
-        post = get_object_or_404(Post, pk=int(kwargs['pk']))  # get the post with that particular chosen ID
-        pk = int(kwargs['pk'])
-        posts = Post.objects.filter(status=False, author_id=request.user.id).exclude(id=pk)
-        # Paginate the draft posts, 5 per page
-        paginator = Paginator(posts, 5)
-        page = request.GET.get('page')  # < Get the page number
-        posts = paginator.get_page(page)  #
-        form = PostForm(instance=post)
-        user = get_user_type(request)
-        args = {'form': form, 'posts': posts, 'user': user}
-        return render(request, self.template_name, args)
-
-    # Edit post information and save changes to database
-    def post(self, request, **kwargs):
-        post = get_object_or_404(Post, pk=int(kwargs['pk']))
-        if request.method == "POST":
-            form = PostForm(request.POST, instance=post)
-            # submit the edited post
-            if request.POST.get("submitbutton"):
-                if form.is_valid():
-                    if post.status:
-                        post = form.save(commit=False)
-                        post.author_updated = request.user
-                        post.update_date = timezone.now()
-                        post.status = True  # edited post is now published
-                        post.save()
-                        return redirect('post_new')
-                    else:
-                        post = form.save(commit=False)
-                        post.author_updated = request.user
-                        post.release_date = timezone.now()
-                        post.update_date = timezone.now()
-                        post.status = True  # edited post is now published
-                        post.save()
-                        return redirect('post_new')
-            # check if post is saved in draft mode
-            elif request.POST.get("savebutton"):
-                if form.is_valid():
-                    post = form.save(commit=False)
-                    post.author_updated = request.user
-                    post.release_date = timezone.now()
-                    post.update_date = timezone.now()
-                    post.status = False
                     post.save()
                     return redirect('post_new')
 
