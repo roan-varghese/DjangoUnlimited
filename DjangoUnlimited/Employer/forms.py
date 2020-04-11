33bec4b28cecbbe2dbf54cac40d0056c2f88da73
Mariyam Iftikhar <33276007@student.murdoch.edu.au>
2020-04-11 19:56:25 +0400

    Added check on phone number for employer form. Fixed filter jobs. Added Create Post button on Bulletin page for admins. Added job status on browse jobs page for admins. Changed some urls. Changed some css stuff.

--- DjangoUnlimited/Employer/forms.py
+++ DjangoUnlimited/Employer/forms.py
@@ -69,8 +69,8 @@ class EmployerForm(forms.ModelForm):
     company_name = forms.CharField(max_length=50, label='*Company Name', required=True, widget=forms.TextInput(
                                                                 attrs={'class': 'form-control-text', 'style': 'resize:none;'}))
     company_description = forms.CharField(label='Company Description', required=False, widget=forms.Textarea)
-    phone_number = forms.CharField(required=False, max_length=15, widget=forms.TextInput(
-                                                                                attrs={'class': 'form-control-text', 'style': 'resize:none;'}))
+    phone_number = forms.CharField(label="Contact Number", required=False, max_length=15, widget=forms.TextInput(
+                                                                                attrs={'class': 'form-control-text', 'style': 'resize:none;', 'id':"num"}))
 
     class Meta:
         model = Employer
